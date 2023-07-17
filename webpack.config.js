80b8587f0501f96c7f87f15ec6ab7fb76dabbd76
jennifertrin <jennifer.tran@dfinity.org>
2023-07-17 15:56:12 -0700

    chore: add babel/preset-typescript

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,8 @@ module.exports = {
               presets: [
                 '@babel/preset-env',
                 '@babel/preset-react',
-                '@babel/preset-flow'
+                '@babel/preset-flow',
+                '@babel/preset-typescript'
               ],
             },
           },
